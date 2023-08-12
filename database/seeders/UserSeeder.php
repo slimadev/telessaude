496b3b63955b1465853dfa48224d7f1b1d8028b0
slimaelixir <slima@elixir.co.mz>
2023-08-12 14:40:30 +0200

    Criacao de utilizador

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,10 +15,17 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         error_log('Rodando a criacao de utilizador.....');
+        $province_id = DB::table('simple_entities')->where('code', '10')->value('id');
+        $framing_id = DB::table('simple_entities')->where('code', '20')->value('id');
         DB::table('users')->insert([
-            'name' => 'Administrador',
-            'email' => 'admin@telessaude.com',
+            'name' => 'Super User',
+            'is_admin' => 1,
+            'is_male' => 1,
+            'province_id'=>$province_id,
+            'framing_id'=>$framing_id,
+            'email' => 'su@telessaude.com',
             'password' => Hash::make('admin'),
         ]);
+        error_log('Utilizador criado.....');
     }
 }
